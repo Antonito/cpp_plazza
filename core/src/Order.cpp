301a49ce1dad9d33cb86168fdd210f33bbd2d94c
Ludovic Petrenko <ludovic.petrenko@epitech.eu>
2017-04-19 04:56:57 +0200

    Added endianness conversion

--- core/src/Order.cpp
+++ core/src/Order.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+#include <arpa/inet.h>
 #include "Order.hpp"
 
 Order::Order() : m_files(), m_info(Information::PHONE_NUMBER)
@@ -51,77 +52,84 @@ Information Order::getInfo() const
 
 std::unique_ptr<uint8_t[]> Order::serialize(size_t &sizeToFill) const
 {
+  uint32_t toSend;
   sizeToFill = 0;
 
   // Size of the fileCount (number of files)
-  sizeToFill += sizeof(size_t);
+  sizeToFill += sizeof(uint32_t);
 
   // Size of each string (string_size + string itself)
   for (std::string const &s : m_files)
     {
-      sizeToFill += sizeof(size_t) + s.size();
+      sizeToFill += sizeof(uint32_t) + s.size();
     }
 
   // Size of information
-  sizeToFill += sizeof(Information);
+  sizeToFill += sizeof(uint32_t);
 
   // Allocation
   std::unique_ptr<uint8_t[]> serial(new uint8_t[sizeToFill]);
-  size_t                     cursor = 0;
-  size_t                     fileCount = m_files.size();
+  uint32_t                   cursor = 0;
+  uint32_t                   fileCount = m_files.size();
 
   // Copy the file count
-  std::memcpy(&serial[cursor], &fileCount, sizeof(size_t));
-  cursor += sizeof(size_t);
+  toSend = htonl(fileCount);
+  std::memcpy(&serial[cursor], &toSend, sizeof(uint32_t));
+  cursor += sizeof(uint32_t);
 
   // Copy each string
   for (std::string const &s : m_files)
     {
-      size_t size = s.size();
+      uint32_t size = s.size();
 
       // Size of the string
-      std::memcpy(&serial[cursor], &size, sizeof(size_t));
-      cursor += sizeof(size_t);
+      toSend = htonl(size);
+      std::memcpy(&serial[cursor], &toSend, sizeof(uint32_t));
+      cursor += sizeof(uint32_t);
       // The string itself
       std::memcpy(&serial[cursor], s.c_str(), size);
       cursor += size;
     }
 
   // Copy the information
-  std::memcpy(&serial[cursor], &m_info, sizeof(Information));
+  toSend = htonl(static_cast<uint32_t>(m_info));
+  std::memcpy(&serial[cursor], &toSend, sizeof(uint32_t));
 
   return (serial);
 }
 
 void Order::deserialize(size_t size, uint8_t *data)
 {
-  size_t fileCount;
-  size_t cursor = 0;
+  uint32_t toHost;
+  uint32_t fileCount;
+  uint32_t cursor = 0;
 
   // Check if data is big enough
-  if (size < sizeof(size_t))
+  if (size < sizeof(uint32_t))
     {
       throw std::exception();
     }
 
   // Get the file count
-  std::memcpy(&fileCount, &data[cursor], sizeof(size_t));
-  cursor += sizeof(size_t);
+  std::memcpy(&toHost, &data[cursor], sizeof(uint32_t));
+  fileCount = ntonl(toHost);
+  cursor += sizeof(uint32_t);
 
   // Loop on each file
-  for (size_t i = 0; i < fileCount; ++i)
+  for (uint32_t i = 0; i < fileCount; ++i)
     {
-      size_t len;
+      uint32_t len;
 
       // Check if data is big enough
-      if (size - cursor < sizeof(size_t))
+      if (size - cursor < sizeof(uint32_t))
 	{
 	  throw std::exception();
 	}
 
       // Get the string length
-      std::memcpy(&len, &data[cursor], sizeof(size_t));
-      cursor += sizeof(size_t);
+      std::memcpy(&toHost, &data[cursor], sizeof(uint32_t));
+      len = ntonl(toHost);
+      cursor += sizeof(uint32_t);
 
       // Check if data is big enough
       if (size - cursor < len)
@@ -135,29 +143,31 @@ void Order::deserialize(size_t size, uint8_t *data)
     }
 
   // Check if data is big enough
-  if (size - cursor < sizeof(Information))
+  if (size - cursor < sizeof(uint32_t))
     {
       throw std::exception();
     }
 
   // Get the information
-  std::memcpy(&m_info, &data[cursor], sizeof(Information));
+  std::memcpy(&toHost, &data[cursor], sizeof(uint32_t));
+  m_info = static_cast<Information>(ntonl(toHost));
 }
 
 bool Order::parse(Order &order, std::stringstream &input)
 {
-  std::string                  curWord;
-  char c;
-  char                         lastChar = ' ';
+  std::string              curWord;
+  char                     c;
+  char                     lastChar = ' ';
   static const std::string info[] = {"PHONE_NUMBER", "EMAIL_ADDRESS",
-                                         "IP_ADDRESS"};
+                                     "IP_ADDRESS"};
   int addedFiles = 0;
 
   order.m_files.clear();
 
   while (input.get(c) && c != ';')
     {
-      if ((lastChar == ' ' || lastChar == '\t') && (c != ' ' && c != '\t') && curWord.length() != 0)
+      if ((lastChar == ' ' || lastChar == '\t') && (c != ' ' && c != '\t') &&
+          curWord.length() != 0)
 	{
 	  order.m_files.push_back(std::move(curWord));
 	  curWord = "";
