d00ce7ede9e3eef1c8b9095e1a6908e2b0d5b25d
Arthur Arnaud <arthur.arnaud@epitech.eu>
2017-04-20 22:03:57 +0000

    merge

--- core/src/Worker.cpp
+++ core/src/Worker.cpp
@@ -14,67 +14,66 @@ Worker::~Worker()
 void Worker::exec(Order &order)
 {
   if (order.size() > 0)
-  {
-    setReg(order.getInfo());
-    loadFile(order[0]);
-    uncipher();
-  }
+    {
+      setReg(order.getInfo());
+      loadFile(order[0]);
+      uncipher();
+    }
 }
 
 void Worker::setReg(Information info)
 {
-  static std::vector<std::string> regInfo =
-      {
-          "[a-z]",
-          "[a-zA-Z0-9_.-]+ '@' [a-zA-Z0-9_.-]+", //magic quote ?
-          "[0-255].[0-255].[0-255].[0-255]",
-      };
+  static std::vector<std::string> regInfo = {
+      "[a-z]",
+      "[a-zA-Z0-9_.-]+ '@' [a-zA-Z0-9_.-]+", // magic quote ?
+      "[0-255].[0-255].[0-255].[0-255]",
+  };
   m_reg = regex::Regex(regInfo[static_cast<int>(info)]);
 }
 
 void Worker::loadFile(std::string const &fileName)
 {
   std::stringstream tmp;
-  std::ifstream file;
+  std::ifstream     file;
 
   file.open(fileName.c_str());
   if (file.is_open())
-  {
-    tmp << file.rdbuf();
-    m_data = tmp.str();
-    m_uncipherData = tmp.str();
-  }
-  //std::cout << m_uncipherData << std::endl;
+    {
+      tmp << file.rdbuf();
+      m_data = tmp.str();
+      m_uncipherData = tmp.str();
+    }
+  // std::cout << m_uncipherData << std::endl;
 }
 
 void Worker::uncipher()
 {
   // Check if file is not ciphered
   if (m_reg.search(m_uncipherData))
-  {
-    fillResult();
-    return;
-  }
+    {
+      fillResult();
+      return;
+    }
   std::cout << "NO MATCH" << std::endl;
   // Bruteforce Xor
   for (int8_t i = 0; i < std::numeric_limits<int8_t>::max(); i++)
-  {
-    if (uncipherXor(i))
     {
-      fillResult();
-      return;
+      if (uncipherXor(i))
+	{
+	  fillResult();
+	  return;
+	}
     }
-  }
 
   // Bruteforce Caesar
   for (int16_t i = 0; i < std::numeric_limits<int16_t>::max(); i++)
-  {
-    if (uncipherCaesar(i))
     {
-      fillResult();
-      return;
+      if (uncipherCaesar(i))
+	{
+	  fillResult();
+	  return;
+	}
     }
-  }
 }
 
 void Worker::fillResult()
@@ -83,9 +82,9 @@ void Worker::fillResult()
 
   match = m_reg.getMatch();
   for (int32_t i = 0; i < static_cast<int32_t>(match.size()); i++)
-  {
-    std::cout << match[i] << std::endl;
-  }
+    {
+      std::cout << match[i] << std::endl;
+    }
 }
 
 bool Worker::uncipherXor(uint8_t key)
@@ -100,12 +99,13 @@ bool Worker::uncipherXor(uint8_t key)
 bool Worker::uncipherCaesar(uint16_t key)
 {
   for (int32_t i = 0; i < static_cast<int32_t>(m_data.size()); i++)
-  {
-    if (static_cast<uint16_t>(m_data[i]) >= key)
-      m_uncipherData[i] = static_cast<char>((m_data[i] - key - 'A') % 26);
-    else
-      m_uncipherData[i] = static_cast<char>((m_data[i] + 26 - (key - 'A')) % 26);
-  }
+    {
+      if (static_cast<uint16_t>(m_data[i]) >= key)
+	m_uncipherData[i] = static_cast<char>((m_data[i] - key - 'A') % 26);
+      else
+	m_uncipherData[i] =
+	    static_cast<char>((m_data[i] + 26 - (key - 'A')) % 26);
+    }
   if (m_reg.search(m_uncipherData))
     return true;
   return false;
