e53f8ccdeb203291f451527522e0c3c73c4ba8dc
Antoine Baché <antoine.bache@epitech.eu>
2017-04-20 14:02:55 +0200

    Added pipe and internet ipcs

--- ipcs/src/InternetSocket.hpp
+++ ipcs/src/InternetSocket.hpp
@@ -6,10 +6,23 @@
 class InternetSocket : public FileDescriptorCommunicable
 {
 public:
+  InternetSocket();
   virtual ~InternetSocket(){};
 
   virtual bool write(IMessage const &);
   virtual bool read(IMessage &);
+  virtual void configureClient();
+  virtual void configureHost();
+
+private:
+  static constexpr size_t buffSize = 4096;
+  enum
+  {
+    SOCK_HOST = 0,
+    SOCK_CLIENT,
+    NB_SOCKS
+  };
+  int m_socks[NB_SOCKS];
 };
 
 #endif // !INTERNET_SOCKET_HPP_
